657f3ec7f8e99a8ea2ec48891e1d7edd7aaff8b6
arieldeveloper <31148540+arieldeveloper@users.noreply.github.com>
2017-08-30 22:43:45 -0400

    added a couple segues

---
+++ Smack-App-New/Controller/CreateAccountVC.swift
@@ -0,0 +1,19 @@
+//
+//  CreateAccountVC.swift
+//  Smack-App-New
+//
+//  Created by Ariel Chouminov on 2017-08-30.
+//  Copyright © 2017 Ariel Chouminov. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
--- Smack-App-New/Utilities/Constants.swift
+++ Smack-App-New/Utilities/Constants.swift
@@ -6,7 +6,10 @@
 //  Copyright © 2017 Ariel Chouminov. All rights reserved.
 //
 
+
+//File defition: This file stores the item identifiers into capitilized constants in order to have all the constants in one file.
 import Foundation
 
-//Segues
+//Segues: These identifiers are found in the storyboard and then referenced in the perform for segue function in the VC's
 let TO_LOGIN = "toLogin"
+let TO_CREATE_ACCOUNT = "toCreateAccount"
