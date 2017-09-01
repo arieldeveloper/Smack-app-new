0e90558e4216c9e9bbca6d55010236860cb8eee8
arieldeveloper <31148540+arieldeveloper@users.noreply.github.com>
2017-09-01 14:42:41 -0400

    added some api functions and created some constants

--- Smack-App-New/Utilities/Constants.swift
+++ Smack-App-New/Utilities/Constants.swift
@@ -10,7 +10,17 @@
 //File defition: This file stores the item identifiers into capitilized constants in order to have all the constants in one file.
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+//URL Constants
+let BASE_URL = " https://chattychatac.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
 //Segues: These identifiers are found in the storyboard and then referenced in the perform for segue function in the VC's
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+
+//user defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
